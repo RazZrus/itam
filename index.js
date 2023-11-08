d230034fdb150f85a7d7b551a8ef5aa006248409
RazZrus <smotrdima@gmail.com>
2023-11-08 13:14:46 +0300

    Mobile menu fixed
    Fixed a bug with mobile menu.
    When you open menu now you can't scroll page

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const burgerMenuButton = document.querySelector('.mobile-menu-button');
 const mobileNavigation = document.querySelector('.mobile-nav');
 const mobileNavigationButton = document.querySelectorAll('.mobile-nav-list-item');
+const page = document.querySelector('body');
 
 burgerMenuButton.addEventListener('click', function () {
 	burgerMenuButton.classList.toggle('is-active');
 	mobileNavigation.classList.toggle('activated');
+	page.classList.toggle('fixed');
 });
 
 console.log(mobileNavigationButton);
@@ -13,5 +15,6 @@ mobileNavigationButton.forEach((button) => {
 	button.addEventListener('click', function () {
 		burgerMenuButton.classList.remove('is-active');
 		mobileNavigation.classList.remove('activated');
+		page.classList.toggle('fixed');
 	});
 });
